303b16709c8d079efe928305d72038f0d2c0bc6b
Zaahiid <zahidrsheikh01@gmail.com>
2024-01-02 12:51:26 +0530

    added props to Rating component

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,26 @@
-import React from 'react'
+import React,{useState} from 'react'
 import ReactDOM from 'react-dom/client'
 // import App from './App.jsx'
 // import './index.css'
 import StarRating from './StarRating.jsx'
 
+function Test () {
+  const [movieRating, setMovieRating] = useState(0)
+  return(
+    <div>
+      <StarRating color='blue' maxRating={10} onSetRating={setMovieRating}/>
+      <p>This movie was rated {movieRating} stars.</p>
+    </div>
+  )
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5}/>
+    <StarRating maxRating={5} messages={["Terrible","Bad", "Okay","Good","Amazing"]}/>
     {/* <StarRating maxRating={10}/> */}
-    {/* <StarRating /> */}
+    <StarRating size={20} color='red' className="Test" defaultRating={3} />
+    <Test/>
 
   </React.StrictMode>,
 )
